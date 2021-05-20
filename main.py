3a938f7466bdb4aa7553ed56a96506a97db08162
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-20 14:19:05 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+cont = int(0)
+numero = int(1)
+casos = int(input())
+while (cont < casos):
+    print("{} {} {} PUM".format(numero, numero+1, numero+2))
+    numero = numero + 4
+    cont = cont + 1
